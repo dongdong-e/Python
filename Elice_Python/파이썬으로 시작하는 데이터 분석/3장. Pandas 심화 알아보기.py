4d7add019b7b91b7ad0823d514a08909c23c3d47
Youngdong Kim <42408554+Youngdong88@users.noreply.github.com>
2019-03-11 11:00:25 +0900

    Update 3장. Pandas 심화 알아보기.py

--- Elice_Python/파이썬으로 시작하는 데이터 분석/3장. Pandas 심화 알아보기.py
+++ Elice_Python/파이썬으로 시작하는 데이터 분석/3장. Pandas 심화 알아보기.py
@@ -1,1 +1,50 @@
+##### 조건으로 검색하기
+
+animal_df = pd.DataFrame({
+    'Animal': ['Dog', 'Cat', 'Cat', 'Pig', 'Cat'],
+    'Name': ['Happy', 'Sam', 'Toby', 'Mini', 'Rocky']
+})
+
+animal_df.Animal.str.match("Cat")
+animal_df["Animal"].str.contains("Cat")
+
+##### 함수로 데이터 처리하기
+
+df = pd.DataFrame(np.arange(5), columns = ["Num"])
+
+# 함수를 만들어 apply 사용하기 (1)
+def square(x):
+    return x ** 2
+df["Num"].apply(square)
+df["Square"] = df["Num"].apply(square)
+
+# 함수를 만들어 apply 사용하기 (2)
+
+df = pd.DataFrame(columns = ["phone"])
+df.loc[0] = "010-1234-1235"
+df.loc[1] = "공일공-일이삼사-1235"
+df.loc[2] = "010.1234.1235"
+df.loc[3] = "공1공-1234.1이3오"
+df["preprocess_phone"] = ''
+
+def get_preprocess_phone(phone):
+    mapping_dict = {
+        "공": "0",
+        "일": "1",
+        "이": "2",
+        "삼": "3",
+        "사": "4",
+        "오": "5",
+        "-": "",
+        ".": ""
+    }
+    for key, value in mapping_dict.items():
+        phone = phone.replace(key, value)
+    return phone
+
+df["preprocess_phone"] = df["phone"].apply(get_preprocess_phone)
+df
+
+# lambda 표현식 사용하기
+df["Square"] = df.Num.apply(lambda x: x ** 2)
 
